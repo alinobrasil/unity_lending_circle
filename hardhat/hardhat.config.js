eab0f75b38eda54d2c5271b4bee1448238ab1abc
Ali Kim <alikim@gmail.com>
2023-10-18 09:51:17 -0400

    deployed to Scroll & Mantle testnets

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" })
+
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY_0]
+    },
+
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts: [process.env.PRIVATE_KEY_0]
+    },
+
+    mantleTest: {
+      url: "https://rpc.testnet.mantle.xyz", // testnet
+      accounts: [process.env.PRIVATE_KEY_0]
+    }
+
+  }
 };
